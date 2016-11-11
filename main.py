cdbf78e6f7c814f91b7c56af6bbff451cfd51357
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-11 22:57:06 +0530

    Cleaner and Efficient data handling. Added Port summary

--- main.py
+++ main.py
@@ -7,67 +7,39 @@ class InternetMeasurements:
     def __init__(self):
         self.dataframe = pandas.read_csv(config.FLOW_RECORD_FILE)
         self.datadescribe = self.dataframe.describe()
+        self.dataframe['flow_diff'] = self.dataframe['last'] - self.dataframe['first']
 
     def average_packet_size(self):
         mean_dpkts = self.datadescribe['dpkts']['mean']
         mean_doctets = self.datadescribe['doctets']['mean']
         return mean_doctets/mean_dpkts
 
-    def plot_ccdf(self):
-        self.dataframe['flow_diff'] = self.dataframe['last'] - self.dataframe['first']
-        X = np.array([i for i in range(len(self.dataframe['flow_diff']))])
-        flow_diff = np.array([self.dataframe['flow_diff'][i] for i in range(len(self.dataframe['flow_diff']))])
-        doctets = np.array([self.dataframe['doctets'][i] for i in range(len(self.dataframe['flow_diff']))])
-        dpkts = np.array([self.dataframe['dpkts'][i] for i in range(len(self.dataframe['flow_diff']))])
-
-        flow_diff = flow_diff/flow_diff.sum()
-        doctets = doctets/doctets.sum()
-        dpkts = dpkts/dpkts.sum()
-
-        C_flow_diff = np.cumsum(flow_diff)
-        C_doctets = np.cumsum(doctets)
-        C_dpkts = np.cumsum(dpkts)
+    def plot_ccdf(self, column):
+        column_data = np.array(self.dataframe[column].tolist())
+        column_data = column_data/column_data.sum()
+        column_cdf  = np.cumsum(column_data)
 
         fig, ax = plt.subplots()
-        ax.set_title("Flow Diff CCDF")
-        ax.plot(X,1-C_flow_diff)
-        fig.savefig('flow_diff_ccdf.png')
-
-
-        fig, ax = plt.subplots()
-        ax.set_title("Dpkts CCDF")
-        ax.plot(X,1-C_dpkts)
-        fig.savefig('dpkts_ccdf.png')
-
-
-        fig, ax = plt.subplots()
-        ax.set_title("Dockets CCDF")
-        ax.plot(X,1-C_doctets)
-        fig.savefig('docktets_ccdf.png')
-
-
+        ax.set_title(column + " CCDF")
+        ax.plot(self.dataframe.index,1-column_cdf)
+        fig.savefig(column + ' ccdf.png')
 
         fig, ax = plt.subplots()
         ax.set_xscale('log')
         ax.set_yscale('log')
-        ax.set_title("Flow Diff CCDF - log")
-        ax.plot(X,1-C_flow_diff)
-        fig.savefig('flow_diff_ccdf_log.png')
+        ax.set_title(column + " CCDF" + " - Log Scale")
+        ax.plot(self.dataframe.index,1-column_cdf)
+        fig.savefig(column + ' ccdf log.png')
 
-        fig, ax = plt.subplots()
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        ax.set_title("Dpkts CCDF - log")
-        ax.plot(X,1-C_dpkts)
-        fig.savefig('dpkts_ccdf_log.png')
-
-        fig, ax = plt.subplots()
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        ax.set_title("Dockets CCDF - log")
-        ax.plot(X,1-C_doctets)
-        fig.savefig('docktets_ccdf_log.png')
-
+    def print_port_summary(self):
+        print("Top 10 - Sender Traffic")
+        print("Port\tCounts")
+        print(self.dataframe['srcport'].value_counts()[:10])
+        print("\n")
+        print("Top 10 - Receiver Traffic")
+        print("Port\tCounts")
+        print(self.dataframe['dstport'].value_counts()[:10])
 
 im = InternetMeasurements()
-im.plot_ccdf()
+# im.plot_ccdf('flow_diff')
+im.print_port_summary()
