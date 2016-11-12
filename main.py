29177ee0307e892d02704116dc1665fc5b33c04e
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-12 21:12:08 +0530

    Added princeton share

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import numpy as np
 class InternetMeasurements:
     def __init__(self):
         self.dataframe = pandas.read_csv(config.FLOW_RECORD_FILE)
-        self.datadescribe = self.dataframe.describe()
-        self.dataframe['flow_diff'] = self.dataframe['last'] - self.dataframe['first']
+        
 
     def average_packet_size(self):
+        self.datadescribe = self.dataframe.describe()
+        self.dataframe['flow_diff'] = self.dataframe['last'] - self.dataframe['first']
         mean_dpkts = self.datadescribe['dpkts']['mean']
         mean_doctets = self.datadescribe['doctets']['mean']
         return mean_doctets/mean_dpkts
@@ -53,7 +54,40 @@ class InternetMeasurements:
         for w in sorted(dst_port_bytes, key=dst_port_bytes.get, reverse=True)[:10]:
             print(w,'\t', dst_port_bytes[w])
 
+    def get_princeton_share(self):
+        data = self.dataframe
+        src_doctets=0
+        src_dpkts=0
+        dst_doctets=0
+        dst_dpkts=0
+        net_doctets=0
+        net_dpkts=0
+        for i in data.index:
+            if data['srcaddr'][i].split('.')[:2] == ['128','112']:
+                src_doctets = src_doctets + data['doctets'][i]
+                src_dpkts   = src_dpkts   + data['dpkts'][i]
+            
+            if data['dstaddr'][i].split('.')[:2] == ['128','112']:
+                dst_doctets = dst_doctets + data['doctets'][i]
+                dst_dpkts   = dst_dpkts   + data['dpkts'][i]
+            
+            net_doctets = net_doctets + data['doctets'][i]
+            net_dpkts   = net_dpkts   + data['dpkts'][i]
+        print('net doctets\t',net_doctets)
+        print('net dpkts\t',net_dpkts)
+        print('*'*16)
+        print('src docktets\t',src_doctets)
+        print('src doctets fraction\t',src_doctets/net_doctets)
+        print('src dpkts\t',src_dpkts)
+        print('src dpkts fraction\t',src_dpkts/net_dpkts)
+        print('*'*16)
+        print('dst docktets\t',dst_doctets)
+        print('dst doctets fraction\t',dst_doctets/net_doctets)
+        print('dst dpkts\t',dst_dpkts)
+        print('dst dpkts fraction\t',dst_dpkts/net_dpkts)
+        
 
 im = InternetMeasurements()
 # im.plot_ccdf('flow_diff')
-im.print_port_summary()
+# im.print_port_summary()
+im.get_princeton_share()
