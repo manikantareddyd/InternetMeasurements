11f1d58cb5e6a1c3c101de3f500a667fdf81f976
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-14 15:58:59 +0530

    Finished BGP

--- bgp_measurements.py
+++ bgp_measurements.py
@@ -3,6 +3,7 @@ import config
 import csv
 from prettytable import PrettyTable
 import operator
+import numpy as np
 class BGPMeasurements:
     def __init__(self):
         self.sessions=["20140103","20140203","20140303"]
@@ -17,16 +18,26 @@ class BGPMeasurements:
             for session in self.sessions:
                 if session in filename:
                     bgp_files_in_updates[session] += 1
-            with open(config.UPDATES_DIR + filename, "r") as infile:
+            with open(
+                config.UPDATES_DIR + filename,
+                "r") as infile:
                 for line in infile:
                     fields = line.split("|")
                     if "." in fields[5]:
                         for session in self.sessions:
                             if session in filename:
                                 bgp_updates[session] += 1
-        x = PrettyTable(["Session","Average Updates/Minute"])
+        x = PrettyTable([
+            "Session",
+            "Average Updates/Minute"
+            ])
         for session in self.sessions:
-            x.add_row([session, bgp_updates[session]/(15*bgp_files_in_updates[session])])
+            minutes = 15*bgp_files_in_updates[session]
+            fraction = bgp_updates[session]/(minutes)
+            x.add_row([
+                session, 
+                fraction
+                ])
         print(x)
 
     def compute_prefix_fractions(self):
@@ -38,7 +49,9 @@ class BGPMeasurements:
             total_updates[session] = 0
         
         for filename in os.listdir(config.RIB_DIR):
-            with open(config.RIB_DIR + filename, "r") as infile:
+            with open(
+                config.RIB_DIR + filename, 
+                "r") as infile:
                 for line in infile:
                     prefix = line.split("|")[5].strip()
                     if "." in prefix:
@@ -47,7 +60,9 @@ class BGPMeasurements:
                                     prefix_list[session][prefix] = 0
         
         for filename in os.listdir(config.UPDATES_DIR):
-            with open(config.UPDATES_DIR + filename,"r") as infile:
+            with open(
+                config.UPDATES_DIR + filename,
+                "r") as infile:
                 for line in infile:
                     prefix = line.split("|")[5].strip()
                     if "." in prefix:
@@ -61,24 +76,65 @@ class BGPMeasurements:
         
         for session in self.sessions:
             print("\nSession:\t",session)
-            x = PrettyTable(["IP Prefix","Fraction of Updates"])
+            x = PrettyTable([
+                "IP Prefix",
+                "Fraction of Updates"
+                ])
             for prefix in prefix_list[session]:
-                prefix_list[session][prefix] = 100*prefix_list[session][prefix]/total_updates[session]
-            for row in sorted(prefix_list[session].items(), key=operator.itemgetter(1), reverse=True)[:3]:
+                counts = prefix_list[session][prefix]
+                total  = total_updates[session]
+                prefix_list[session][prefix] = 100*counts/total
+            for row in sorted(
+                prefix_list[session].items(), 
+                key=operator.itemgetter(1), 
+                reverse=True
+                )[:3]:
                 x.add_row(row)
             print(x)
         self.prefix_list = prefix_list
             
     def compute_no_update_fraction(self):
         self.compute_prefix_fractions()
-        x = PrettyTable(["Session","0 update Prefixes", "Total Prefixes", "% Fraction"])
+        x = PrettyTable([
+            "Session",
+            "0 update Prefixes", 
+            "Total Prefixes", 
+            "% Fraction"
+            ])
         for session in self.sessions:
             zero_count = 0
             for prefix in self.prefix_list[session]:
                 if self.prefix_list[session][prefix] != 0:
                     zero_count += 1
-            x.add_row([session,zero_count,len(self.prefix_list[session]),100*zero_count/len(self.prefix_list[session])])
+            total_prefixes = len(self.prefix_list[session])
+            fraction = 100*zero_count/total_prefixes
+            x.add_row([
+                session,
+                zero_count,
+                total_prefixes,
+                fraction,
+                ])
         print(x)
-a = BGPMeasurements()
-a.compute_no_update_fraction()
-# a.compute_updates()
+    
+    def compute_distibution_unstable_prefixes(self):
+        self.compute_prefix_fractions()
+        print("\nTop shares of updates")
+        for session in self.sessions:
+            fraction_list = np.array(
+                sorted(
+                    self.prefix_list[session].items(), 
+                    key=operator.itemgetter(1), 
+                    reverse=True)
+                    )[:,1].astype(float)
+            _0_1 = int(len(fraction_list)*0.1/100)
+            _1_0 = int(len(fraction_list)*1/100)
+            _10_0 = int(len(fraction_list)*10/100)
+            top_0_1 = fraction_list[:_0_1].sum()
+            top_1_0 = fraction_list[:_1_0].sum()
+            top_10_0 = fraction_list[:_10_0].sum()
+            print("\nSession",session)
+            x = PrettyTable(["Top","Updates %"])
+            x.add_row(["0.1%",top_0_1])
+            x.add_row(["1%",top_1_0])
+            x.add_row(["10%",top_10_0])
+            print(x)
